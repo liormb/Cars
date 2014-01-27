61e975b6d4624fd0b466ee07353bb5f46a30bea2
liormb <liormb@gmail.com>
2014-01-27 13:46:48 -0500

    Initialize rails cars project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cars::Application.config.secret_token = '739bf1324040baa51ac92fa956e98ceeb98e1f3e15ab1aa61495fdfcce02c3b2ad6bd32d7c77b39ce00a5fdaaa6148ef42caae6a2e6fda07925f7a5f319be7a2'
