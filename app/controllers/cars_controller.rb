0aa2edf975e7493b852315693cd0b918544a2d6d
Isaac50 <irt007@gmail.com>
2014-01-27 14:53:37 -0500

    create car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,12 +3,20 @@ class CarsController < ApplicationController
 	end
 
 	def new
+
 	end
 
 	def edit
 	end
 
 	def create
+		make = params[:make]
+		model = params[:model]
+		year = params[:year]		
+		color = params[:color]
+
+		car = Car.create(make: make, model: model, year: year, color: color)
+		redirect_to car_path(car.id)
 	end
 
 	def show
