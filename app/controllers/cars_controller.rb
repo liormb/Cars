a4153afb9de0fdfa4fed3da7926049be799d4335
liormb <liormb@gmail.com>
2014-01-29 14:17:41 -0500

    adding edit page

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,16 +1,20 @@
 class CarsController < ApplicationController
-	def index # Lior
+	def index
 		@cars = Car.all
 	end
 
-	def new # Isaac
+	def new
  	end
 
-	def edit # Serge
+ 	def show
 		@car = Car.find(params[:id])
 	end
 
-	def create # Isaac
+	def edit
+		@car = Car.find(params[:id])
+	end
+
+	def create
 		make = params[:make]
 		model = params[:model]
 		year = params[:year]		
@@ -20,15 +24,18 @@ class CarsController < ApplicationController
 		redirect_to car_path(car.id)
 	end
 
-	def show # Serge
-		@car = Car.find(params[:id])
+	def update
+		car = Car.find(params[:id])
+		car.make = params[:make]
+		car.model = params[:model]
+		car.year = params[:year]		
+		car.color = params[:color]
+		car.save
+
+		redirect_to "/cars/#{car.id}"
 	end
 
-	def update # Serge
-		@car = Car.find(params[:id])
-	end
-
-	def destroy # Lior
+	def destroy
 		Car.find(params[:id]).destroy
 		redirect_to cars_url
 	end
